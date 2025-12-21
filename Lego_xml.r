8722aa0d4ebf5d1b8e382b92839b0ac7f55b7f45
Brian Holt <brian.holt@seattlecolleges.edu>
2025-12-21 11:01:31 -0800

    added text scrape

--- Lego_xml.r
+++ Lego_xml.r
@@ -1,8 +1,10 @@
 
 
 library(xml2)
+library(clipr)
 
 lego <- read_xml(x ="/home/brian/Downloads/hilux_Parts.xml")
 
 xml_structure(lego)
-xml_find_all(lego,"//ITEMID")
+item_numbers <- xml_find_all(lego,"//ITEMID")
+write_clip( xml_text(item_numbers, trim = TRUE))
